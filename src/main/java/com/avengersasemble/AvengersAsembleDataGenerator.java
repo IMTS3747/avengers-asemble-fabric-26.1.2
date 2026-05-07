041db1cbccb1a0365902a5dd9bfea53b5590c49b
IMTS37 <imts49504@gmail.com>
2026-05-07 15:04:46 +0600

    Initial commit

---
+++ src/main/java/com/avengersasemble/AvengersAsembleDataGenerator.java
@@ -0,0 +1,11 @@
+package com.avengersasemble;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AvengersAsembleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
